37958c6b31acf01988a0a7d13f2261a08cd0b602
Ben Pirt <ben@pirt.co.uk>
2020-04-21 10:14:07 +0100

    Fix formatting

--- lambda.js
+++ lambda.js
@@ -1,67 +1,67 @@
-const fs = require("fs");
-const { spawn } = require("child_process");
-const { inlineSource } = require("inline-source");
+const fs = require('fs');
+const { spawn } = require('child_process');
+const { inlineSource } = require('inline-source');
 const statusCodes = {
-  415: "Unsupported Media Type",
-  500: "Internal Server Error",
+  415: 'Unsupported Media Type',
+  500: 'Internal Server Error'
 };
 
 const send = (statusCode, contentType, body) => {
   return {
     statusCode,
-    headers: { "Content-Type": contentType },
+    headers: { 'Content-Type': contentType },
     isBase64Encoded: false,
-    body,
+    body
   };
 };
 
-const send_html = (body) => {
-  return send(200, "text/html", body);
+const send_html = body => {
+  return send(200, 'text/html', body);
 };
 
 const send_error = (statusCode, message) => {
   return send(
     statusCode,
-    "application/json",
+    'application/json',
     JSON.stringify({ statusCode, error: statusCodes[statusCode], message })
   );
 };
 
-const inlineImages = async (path) => {
+const inlineImages = async path => {
   return inlineSource(path, {
     compress: true,
     attribute: false,
-    rootpath: "/tmp",
+    rootpath: '/tmp'
   });
 };
 
-const convertRtf = async (rtf) => {
-  fs.writeFileSync("/tmp/temp.rtf", rtf);
-  let out = "";
-  let error = "";
+const convertRtf = async rtf => {
+  fs.writeFileSync('/tmp/temp.rtf', rtf);
+  let out = '';
+  let error = '';
   return new Promise((resolve, reject) => {
     const converter = spawn(
-      "/opt/bin/perl",
-      ["./rtf2html.pl", "/tmp/temp.rtf", "/tmp/temp.html"],
+      '/opt/bin/perl',
+      ['./rtf2html.pl', '/tmp/temp.rtf', '/tmp/temp.html'],
       {
-        cwd: "./perl",
+        cwd: './perl'
       }
     );
-    converter.stderr.on("data", (data) => (error += data));
-    converter.stdout.on("data", (data) => (out += data));
-    converter.on("close", (code) => {
-      if (out !== "") console.log(out);
-      let html = inlineImages("/tmp/temp.html");
+    converter.stderr.on('data', data => (error += data));
+    converter.stdout.on('data', data => (out += data));
+    converter.on('close', code => {
+      if (out !== '') console.log(out);
+      let html = inlineImages('/tmp/temp.html');
       code !== 0 ? reject(error) : resolve(html);
     });
   });
 };
 
-const convert = async (event) => {
+const convert = async event => {
   const contentType =
-    event.headers["content-type"] || event.headers["Content-Type"];
-  if (contentType !== "application/rtf")
-    return send_error(415, "Content type is not RTF");
+    event.headers['content-type'] || event.headers['Content-Type'];
+  if (contentType !== 'application/rtf')
+    return send_error(415, 'Content type is not RTF');
 
   const rtf = event.body;
   try {
@@ -74,11 +74,11 @@ const convert = async (event) => {
 };
 
 const form = () => {
-  const formDoc = fs.readFileSync("./form.html", "utf8");
+  const formDoc = fs.readFileSync('./form.html', 'utf8');
   return send_html(formDoc);
 };
 
-module.exports.handler = async (event) => {
-  if (event.httpMethod === "GET") return form(event);
-  if (event.httpMethod === "POST") return await convert(event);
+module.exports.handler = async event => {
+  if (event.httpMethod === 'GET') return form(event);
+  if (event.httpMethod === 'POST') return await convert(event);
 };
--- test/integration.test.js
+++ test/integration.test.js
@@ -1,42 +1,42 @@
-const fs = require("fs");
-const fetch = require("node-fetch");
-const HtmlDiffer = require("html-differ").HtmlDiffer;
+const fs = require('fs');
+const fetch = require('node-fetch');
+const HtmlDiffer = require('html-differ').HtmlDiffer;
 const headers = {
-  rtf: "application/rtf",
-  html: "text/html",
+  rtf: 'application/rtf',
+  html: 'text/html'
 };
 
-const fileList = (type) => fs.readdirSync(`./test/${type}`);
-const rtfFiles = fileList("rtf");
+const fileList = type => fs.readdirSync(`./test/${type}`);
+const rtfFiles = fileList('rtf');
 
 const readFixture = (type, name) =>
-  fs.readFileSync(`./test/${type}/${name}`).toString("utf8");
+  fs.readFileSync(`./test/${type}/${name}`).toString('utf8');
 
 const convertFile = async (inType, outType, name) => {
   const body = readFixture(inType, name);
-  return await fetch("http://localhost:4000/", {
-    method: "POST",
+  return await fetch('http://localhost:4000/', {
+    method: 'POST',
     body,
     headers: {
-      "Content-Type": headers[inType],
-      Accept: headers[outType],
-    },
-  }).then((res) => res.text());
+      'Content-Type': headers[inType],
+      Accept: headers[outType]
+    }
+  }).then(res => res.text());
 };
 
 const diff = (file1, file2) => {
   const htmlDiffer = new HtmlDiffer();
-  result = htmlDiffer.isEqual(file1, file2);
+  const result = htmlDiffer.isEqual(file1, file2);
   if (!result) console.log(htmlDiffer.diffHtml(file1, file2));
   return result;
 };
 
-describe("Converting RTF to HTML", () => {
+describe('Converting RTF to HTML', () => {
   for (let rtfFile of rtfFiles) {
-    const htmlFile = rtfFile.replace(".rtf", ".html");
+    const htmlFile = rtfFile.replace('.rtf', '.html');
     it(`renders file ${rtfFile} to ${htmlFile}`, async function() {
-      const convertedFile = await convertFile("rtf", "html", rtfFile);
-      const expectedFile = readFixture("html", htmlFile);
+      const convertedFile = await convertFile('rtf', 'html', rtfFile);
+      const expectedFile = readFixture('html', htmlFile);
       expect(diff(convertedFile, expectedFile)).toBe(true);
     });
   }
